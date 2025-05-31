f588e4f8e680f0f80fc8a526336bf7def5747eeb
Bartosz Gordon <gordonbartosz@gmail.com>
2025-05-31 17:33:14 +0200

    step: migrate mime.vo

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
 export * from "./rounding.service";
 export * from "./noop.service";
+export * from "./size.vo";
+export * from "./mime.vo";
---
+++ src/mime.vo.ts
@@ -0,0 +1,49 @@
+export type MimeRawType = string;
+export type MimeTypeType = string;
+export type MimeSubtypeType = string;
+
+export class Mime {
+  readonly raw: MimeRawType;
+  readonly type: MimeTypeType;
+  readonly subtype: MimeSubtypeType;
+
+  constructor(value: MimeRawType) {
+    const [type, subtype] = value.split("/");
+
+    if (typeof type !== "string" || type.length === 0) {
+      throw new InvalidMimeError();
+    }
+
+    if (typeof subtype !== "string" || subtype.length === 0) {
+      throw new InvalidMimeError();
+    }
+
+    this.raw = value;
+    this.type = type;
+    this.subtype = subtype;
+  }
+
+  isSatisfiedBy(another: Mime): boolean {
+    if (this.raw === another.raw) return true;
+
+    const typeMatches = this.type === another.type || this.type === "*";
+    if (!typeMatches) return false;
+
+    return this.subtype === another.subtype || this.subtype === "*";
+  }
+}
+export class InvalidMimeError extends Error {
+  constructor() {
+    super();
+    Object.setPrototypeOf(this, InvalidMimeError.prototype);
+  }
+}
+
+export class NotAcceptedMimeError extends Error {
+  mime: MimeRawType;
+  constructor(mime: MimeRawType) {
+    super();
+    Object.setPrototypeOf(this, NotAcceptedMimeError.prototype);
+    this.mime = mime;
+  }
+}
