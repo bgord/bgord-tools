6d226f07373b8cd4574f64671a0f39ee7e23c318
Bartosz Gordon <gordonbartosz@gmail.com>
2025-05-31 16:12:43 +0200

    step: add rounding service

---
+++ src/rounding.service.ts
@@ -0,0 +1,31 @@
+export abstract class RoundingStrategy {
+  abstract round(value: number): number;
+}
+
+export class RoundToNearest extends RoundingStrategy {
+  round(value: number): number {
+    return Math.round(value);
+  }
+}
+
+export class RoundUp extends RoundingStrategy {
+  round(value: number): number {
+    return Math.ceil(value);
+  }
+}
+
+export class RoundDown extends RoundingStrategy {
+  round(value: number): number {
+    return Math.floor(value);
+  }
+}
+
+export class RoundToDecimal extends RoundingStrategy {
+  constructor(private readonly decimals: number) {
+    super();
+  }
+
+  round(value: number): number {
+    return Number.parseFloat(value.toFixed(this.decimals));
+  }
+}
