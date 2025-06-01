f15d9488c3e0856e6993b9e007d4795c4690bff9
Bartosz Gordon <gordonbartosz@gmail.com>
2025-06-01 14:01:13 +0200

    step: test sleep service

---
+++ tests/bun.sleep.service.test.ts
@@ -0,0 +1,21 @@
+import { describe, expect, it, vi } from "vitest";
+import { sleep } from "../src/sleep.service";
+
+describe("sleep", () => {
+  it("waits for the specified milliseconds", async () => {
+    vi.useFakeTimers();
+    const spy = vi.fn();
+
+    sleep({ ms: 1000 }).then(spy);
+
+    expect(spy).not.toHaveBeenCalled();
+
+    vi.advanceTimersByTime(1000);
+
+    await vi.runAllTicks();
+
+    expect(spy).toHaveBeenCalled();
+
+    vi.useRealTimers();
+  });
+});
