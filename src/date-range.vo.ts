4f03550a027f09922fa1287a1c67297287b58bf3
Bartosz Gordon <gordonbartosz@gmail.com>
2025-07-22 15:51:52 +0200

    step: add date-range

---
+++ src/date-range.vo.ts
@@ -0,0 +1,30 @@
+import { TimestampType } from "./timestamp.vo";
+
+export class DateRange {
+  constructor(
+    private readonly start: TimestampType,
+    private readonly end: TimestampType,
+  ) {
+    if (start > end) throw new Error("Invalid date range");
+  }
+
+  getStart(): TimestampType {
+    return this.start;
+  }
+
+  getEnd(): TimestampType {
+    return this.end;
+  }
+
+  toRange(): [TimestampType, TimestampType] {
+    return [this.start, this.end];
+  }
+
+  contains(timestamp: TimestampType): boolean {
+    return timestamp >= this.start && timestamp <= this.end;
+  }
+
+  equals(other: DateRange): boolean {
+    return this.start === other.start && this.end === other.end;
+  }
+}
