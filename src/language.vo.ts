5b20e435ce90990b0423b5056636b80613096fe3
Bartosz Gordon <gordonbartosz@gmail.com>
2025-08-23 18:36:36 +0200

    refactor: remove language vo brand

--- src/language.vo.ts
+++ src/language.vo.ts
@@ -3,7 +3,6 @@ import { z } from "zod/v4";
 export const Language = z
   .string()
   .length(2)
-  .regex(/^[a-z]{2}$/, { message: "invalid_language" })
-  .brand("Language");
+  .regex(/^[a-z]{2}$/, { message: "invalid_language" });
 
 export type LanguageType = z.infer<typeof Language>;
