8b1509421df5ca89cf40618e83d22833557b196a
Bartosz Gordon <gordonbartosz@gmail.com>
2025-10-02 14:56:26 +0200

    refactor: improve email-mask

--- tests/email-mask.service.test.ts
+++ tests/email-mask.service.test.ts
@@ -3,7 +3,7 @@ import { Email, EmailMask } from "../src/email-mask.service";
 
 describe("EmailMask", () => {
   test("works for 1 char", () => {
-    expect(EmailMask.censor(Email.parse("te@example.com"))).toEqual("**@example.com");
+    expect(EmailMask.censor(Email.parse("t@example.com"))).toEqual("*@example.com");
   });
 
   test("works for 2 chars", () => {
