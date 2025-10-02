9fa2a23787671527021ff05f8d33de5d04caa91f
Bartosz Gordon <gordonbartosz@gmail.com>
2025-10-02 22:07:25 +0200

    refactor: refactor stopwatch

--- src/stopwatch.service.ts
+++ src/stopwatch.service.ts
@@ -1,26 +1,24 @@
 import { Timestamp, type TimestampType } from "./timestamp.vo";
-import type { Falsy } from "./ts-utils";
 
 enum StopwatchState {
   started = "started",
   stopped = "stopped",
 }
 
+export const StopwatchStateError = "stopwatch.already.stopped" as const;
+
 export type StopwatchResultType = { durationMs: TimestampType };
 
 export class Stopwatch {
   private state: StopwatchState = StopwatchState.started;
 
-  private stopMs: Falsy<TimestampType>;
-
   constructor(private readonly startMs: TimestampType) {}
 
   stop(): StopwatchResultType {
-    if (this.state === StopwatchState.stopped) throw new Error("Stopwatch is already stopped");
+    if (this.state === StopwatchState.stopped) throw new Error(StopwatchStateError);
 
     this.state = StopwatchState.stopped;
-    this.stopMs = Timestamp.parse(Date.now());
 
-    return { durationMs: Timestamp.parse(this.stopMs - this.startMs) };
+    return { durationMs: Timestamp.parse(Date.now() - this.startMs) };
   }
 }
--- tests/stopwatch.service.test.ts
+++ tests/stopwatch.service.test.ts
@@ -1,29 +1,25 @@
 import { afterEach, beforeEach, describe, expect, setSystemTime, test } from "bun:test";
-import { Stopwatch } from "../src/stopwatch.service";
-import { Timestamp, type TimestampType } from "../src/timestamp.vo";
+import { Stopwatch, StopwatchStateError } from "../src/stopwatch.service";
+import { Timestamp } from "../src/timestamp.vo";
 
 describe("Stopwatch", () => {
   beforeEach(() => setSystemTime(1_000_000));
   afterEach(() => setSystemTime());
 
   test("calculates duration correctly", () => {
-    const now = 1_000_000 as TimestampType;
+    const now = Timestamp.parse(1_000_000);
     const stopwatch = new Stopwatch(now);
+    setSystemTime(now + 500);
 
-    setSystemTime(1_000_000 + 500);
-
-    const result = stopwatch.stop();
-
-    expect(result.durationMs).toBe(Timestamp.parse(500));
+    expect(stopwatch.stop().durationMs).toBe(Timestamp.parse(500));
   });
 
   test("throws if stop is called twice", () => {
-    const now = 1_000_000 as TimestampType;
+    const now = Timestamp.parse(1_000_000);
     const stopwatch = new Stopwatch(now);
     setSystemTime(1_000_000 + 100);
 
     stopwatch.stop();
-
-    expect(() => stopwatch.stop()).toThrowError("Stopwatch is already stopped");
+    expect(() => stopwatch.stop()).toThrow(StopwatchStateError);
   });
 });
