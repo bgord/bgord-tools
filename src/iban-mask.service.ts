66defe4642bb45e8475ba914134a0a94a3128e54
Bartosz Gordon <gordonbartosz@gmail.com>
2025-10-09 23:19:06 +0200

    refactor: simplify iban mask

--- src/iban-mask.service.ts
+++ src/iban-mask.service.ts
@@ -1,17 +1,14 @@
 import type { IBAN } from "./iban.vo";
 
-type IbanMaskedType = string;
-
-// TODO
 export class IbanMask {
-  static censor(iban: IBAN): IbanMaskedType {
+  static censor(iban: IBAN): string {
     const value = iban.format();
 
     const FIRST_SPACE_INDEX = 4;
-    const LAST_SPACE_START_INDEX = value.length - 5;
+    const LAST_SPACE_INDEX = value.length - 5;
 
     const start = value.slice(0, FIRST_SPACE_INDEX);
-    const middle = value.slice(FIRST_SPACE_INDEX + 1, LAST_SPACE_START_INDEX);
+    const middle = value.slice(FIRST_SPACE_INDEX + 1, LAST_SPACE_INDEX);
     const end = value.slice(-4);
 
     const maskedMiddle = middle.replace(/[A-Z0-9]/g, "*");
