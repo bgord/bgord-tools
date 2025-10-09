420f006eb6e386f8f7c11ab9608f97267b6746d9
Bartosz Gordon <gordonbartosz@gmail.com>
2025-10-09 21:30:46 +0200

    refactor: remove todo

--- src/duration-ms.vo.ts
+++ src/duration-ms.vo.ts
@@ -2,8 +2,6 @@ import { z } from "zod/v4";
 
 export const DurationMsError = { Invalid: "duration.invalid" } as const;
 
-// TODO
-// Positive?
 export const DurationMs = z.number(DurationMsError.Invalid).int(DurationMsError.Invalid).brand("DurationMs");
 
 export type DurationMsType = z.infer<typeof DurationMs>;
