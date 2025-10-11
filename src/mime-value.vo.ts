73316a7a3bd57dd7eef270865d27248c0af7ca83
Bartosz Gordon <gordonbartosz@gmail.com>
2025-10-11 13:52:58 +0200

    refactor: simplify mime value

--- src/mime-value.vo.ts
+++ src/mime-value.vo.ts
@@ -2,14 +2,11 @@ import { z } from "zod/v4";
 
 export const MimeValueError = { Type: "mime.value.type", Invalid: "mime.value.invalid" };
 
+// One to twenty four lowercase letters or an asterisk, forward slash, one to twenty four lowercase letters or an asterisk
 const MIME_VALUE_CHARS_WHITELIST = /^[a-z*]{1,24}\/[a-z*]{1,24}$/;
 
 export const MimeValue = z
   .string(MimeValueError.Type)
   .regex(MIME_VALUE_CHARS_WHITELIST, MimeValueError.Invalid)
-  .transform((value) => {
-    const [type, subtype] = value.split("/");
-
-    return { type, subtype };
-  })
+  .transform((value) => ({ type: value.split("/")[0], subtype: value.split("/")[1] }))
   .brand("MimeValue");
