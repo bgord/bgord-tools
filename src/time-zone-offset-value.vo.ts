d9073f08c2e51333ff044e6c7451f61864b8a499
Bartosz Gordon <gordonbartosz@gmail.com>
2025-10-11 14:16:22 +0200

    refactor: simplify timezone offset value

--- src/time-zone-offset-value.vo.ts
+++ src/time-zone-offset-value.vo.ts
@@ -1,6 +1,5 @@
 import { z } from "zod/v4";
 
-// TODO
 export const TimeZoneOffsetValue = z.coerce.number().catch(0).brand("TimeZoneOffsetValue");
 
 export type TimeZoneOffsetValueType = z.infer<typeof TimeZoneOffsetValue>;
--- tests/time-zone-offset-value.test.ts
+++ tests/time-zone-offset-value.test.ts
@@ -1,24 +1,19 @@
 import { describe, expect, test } from "bun:test";
-import { TimeZoneOffsetValue, type TimeZoneOffsetValueType } from "../src/time-zone-offset-value.vo";
+import { TimeZoneOffsetValue } from "../src/time-zone-offset-value.vo";
 
 describe("TimeZoneOffset", () => {
-  test("parses valid numeric strings", () => {
-    expect(() => TimeZoneOffsetValue.parse("120")).not.toThrow();
-    expect(() => TimeZoneOffsetValue.parse("-60")).not.toThrow();
-    expect(() => TimeZoneOffsetValue.parse("0")).not.toThrow();
+  test("happy path", () => {
+    expect(TimeZoneOffsetValue.safeParse("120").success).toEqual(true);
+    expect(TimeZoneOffsetValue.safeParse("-60").success).toEqual(true);
+    expect(TimeZoneOffsetValue.safeParse("0").success).toEqual(true);
   });
 
-  test("trims the string before parsing", () => {
-    expect(TimeZoneOffsetValue.parse(" 90 ")).toEqual(90 as TimeZoneOffsetValueType);
-  });
-
-  test("transforms undefined to 0", () => {
-    expect(TimeZoneOffsetValue.parse(undefined)).toEqual(0 as TimeZoneOffsetValueType);
-  });
-
-  test("transforms non-numeric strings to 0", () => {
-    expect(TimeZoneOffsetValue.parse("abc")).toEqual(0 as TimeZoneOffsetValueType);
-    expect(TimeZoneOffsetValue.parse("")).toEqual(0 as TimeZoneOffsetValueType);
-    expect(TimeZoneOffsetValue.parse(" ")).toEqual(0 as TimeZoneOffsetValueType);
+  test("transforms invalid values to 0", () => {
+    // @ts-expect-error Value comparison
+    expect(TimeZoneOffsetValue.parse(undefined)).toEqual(0);
+    // @ts-expect-error Value comparison
+    expect(TimeZoneOffsetValue.parse(null)).toEqual(0);
+    // @ts-expect-error Value comparison
+    expect(TimeZoneOffsetValue.parse("")).toEqual(0);
   });
 });
