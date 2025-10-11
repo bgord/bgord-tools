d8392d3828c19442a7374860fe09969b4bc7a70b
Bartosz Gordon <gordonbartosz@gmail.com>
2025-10-11 14:05:17 +0200

    refactor: simplify rate limiter

--- src/rate-limiter.service.ts
+++ src/rate-limiter.service.ts
@@ -6,26 +6,26 @@ type RateLimiterResultErrorType = { allowed: false; remaining: Duration };
 type RateLimiterResultType = RateLimiterResultSuccessType | RateLimiterResultErrorType;
 
 export class RateLimiter {
-  private lastInvocationTimestampMs: TimestampType | null = null;
+  private lastInvocation: TimestampType | null = null;
 
   constructor(private readonly duration: Duration) {}
 
-  verify(currentTimestampMs: TimestampType): RateLimiterResultType {
-    if (this.lastInvocationTimestampMs == null) {
-      this.lastInvocationTimestampMs = currentTimestampMs;
+  verify(now: TimestampType): RateLimiterResultType {
+    if (this.lastInvocation == null) {
+      this.lastInvocation = now;
 
       return { allowed: true };
     }
 
-    const nextAllowedTimestampMs = this.lastInvocationTimestampMs + this.duration.ms;
+    const nextAllowedTimestamp = this.lastInvocation + this.duration.ms;
 
-    if (nextAllowedTimestampMs <= currentTimestampMs) {
-      this.lastInvocationTimestampMs = currentTimestampMs;
+    if (nextAllowedTimestamp <= now) {
+      this.lastInvocation = now;
 
       return { allowed: true };
     }
 
-    const remainingDelta = nextAllowedTimestampMs - currentTimestampMs;
+    const remainingDelta = nextAllowedTimestamp - now;
 
     return { allowed: false, remaining: Duration.Ms(remainingDelta) };
   }
