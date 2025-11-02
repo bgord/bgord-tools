91573cc8aaf4a77e77d9f9a217d1007b2c5c5ec1
Bartosz Gordon <gordonbartosz@gmail.com>
2025-11-02 13:57:19 +0100

    fix: mocks

--- tests/mocks.ts
+++ tests/mocks.ts
@@ -1,8 +1,8 @@
 import { Timestamp } from "../src/timestamp.vo";
 
 // Tue Nov 14 2023 22:13:20 GMT+0000
-export const TIME_ZERO = Timestamp.parse(1700000000000);
+export const TIME_ZERO = Timestamp.fromNumber(1700000000000);
 
 export const TIME_ZERO_DATE = "2023-11-14";
 
-export const toTimestamp = (date: string) => Timestamp.parse(new Date(date).getTime());
+export const toTimestamp = (date: string) => Timestamp.fromNumber(new Date(date).getTime()).get();
