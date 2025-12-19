8a2da214d3e6985ddee3d3ed573da7e694c5f6e1
Bartosz Gordon <gordonbartosz@gmail.com>
2025-12-19 12:20:17 +0100

    fix: make mime types const

--- src/mime-types.vo.ts
+++ src/mime-types.vo.ts
@@ -1,7 +1,7 @@
-export const MIME_TYPES: Record<string, string[]> = {
+export const MIME_TYPES = {
   wildcard: ["*/*"],
   jpeg: ["image/jpeg"],
   png: ["image/png"],
   wav: ["audio/x-wav", "audio/wav"],
   mp4: ["video/mp4"],
-};
+} as const;
