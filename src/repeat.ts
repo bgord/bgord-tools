5255b661b72031094b20a82dc0f655fb550ac4de
Bartosz Gordon <gordonbartosz@gmail.com>
2026-03-05 14:40:38 +0100

    refactor: simplify repeat

--- src/repeat.ts
+++ src/repeat.ts
@@ -1,5 +1,3 @@
-import type { IntegerPositiveType } from "./integer-positive.vo";
-
-export function repeat<T>(value: T, count: IntegerPositiveType): Array<T> {
+export function repeat<T>(value: T, count: number): Array<T> {
   return Array.from({ length: count }).map(() => value);
 }
--- tests/repeat.test.ts
+++ tests/repeat.test.ts
@@ -1,11 +1,10 @@
 import { describe, expect, test } from "bun:test";
-import { IntegerPositive } from "../src/integer-positive.vo";
 import { repeat } from "../src/repeat";
 
 describe("repeat", () => {
   test("happy path", () => {
     const value = "abc";
 
-    expect(repeat(value, IntegerPositive.parse(3))).toEqual([value, value, value]);
+    expect(repeat(value, 3)).toEqual([value, value, value]);
   });
 });
