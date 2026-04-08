7c85917ab31dcdef306c92a9a574e88bab64c908
Bartosz Gordon <gordonbartosz@gmail.com>
2026-04-08 13:30:52 +0200

    step: add date formatter relative

--- src/date-formatter.service.ts
+++ src/date-formatter.service.ts
@@ -1,4 +1,5 @@
 import { Duration } from "./duration.service";
+import { RoundingDownStrategy } from "./rounding-down.strategy";
 import type { Timestamp } from "./timestamp.vo";
 
 export class DateFormatter {
@@ -22,5 +23,17 @@ export class DateFormatter {
     const day = zdt.day.toString().padStart(2, "0");
 
     return `${zdt.year}/${month}/${day}`;
+  }
+
+  static relative(now: Timestamp, timestamp: Timestamp, offset: Duration = Duration.ZERO): string {
+    const rtf = new Intl.RelativeTimeFormat("en-US");
+    const rounding = new RoundingDownStrategy();
+    const difference = now.difference(timestamp.add(offset));
+
+    if (difference.toAbsolute().days >= 1) return rtf.format(-rounding.round(difference.days), "day");
+    if (difference.toAbsolute().hours >= 1) return rtf.format(-rounding.round(difference.hours), "hour");
+    if (difference.toAbsolute().minutes >= 1)
+      return rtf.format(-rounding.round(difference.minutes), "minute");
+    return "just now";
   }
 }
--- tests/date-formatter.service.test.ts
+++ tests/date-formatter.service.test.ts
@@ -4,23 +4,61 @@ import { Duration } from "../src/duration.service";
 import { Temporal } from "../src/temporal";
 import { Timestamp } from "../src/timestamp.vo";
 
-const timestamp = Timestamp.fromInstant(Temporal.Instant.from("2023-01-01T00:01:00Z"));
+const now = Timestamp.fromInstant(Temporal.Instant.from("2023-01-01T00:01:00Z"));
 const offset = Duration.Hours(1);
 
 describe("DateFormatter", () => {
   test("datetime", () => {
-    expect(DateFormatter.datetime(timestamp)).toEqual("2023/01/01 00:01");
+    expect(DateFormatter.datetime(now)).toEqual("2023/01/01 00:01");
   });
 
   test("datetime with offset", () => {
-    expect(DateFormatter.datetime(timestamp, offset)).toEqual("2023/01/01 01:01");
+    expect(DateFormatter.datetime(now, offset)).toEqual("2023/01/01 01:01");
   });
 
   test("date", () => {
-    expect(DateFormatter.date(timestamp)).toEqual("2023/01/01");
+    expect(DateFormatter.date(now)).toEqual("2023/01/01");
   });
 
-  test("datewith offset", () => {
-    expect(DateFormatter.date(timestamp, offset)).toEqual("2023/01/01");
+  test("date with offset", () => {
+    expect(DateFormatter.date(now, offset)).toEqual("2023/01/01");
+  });
+
+  test("relative - just now", () => {
+    expect(DateFormatter.relative(now, now)).toEqual("just now");
+  });
+
+  test("relative - ago - minute", () => {
+    expect(DateFormatter.relative(now, now.subtract(Duration.Minutes(1)))).toEqual("1 minute ago");
+    expect(DateFormatter.relative(now, now.subtract(Duration.Minutes(2)))).toEqual("2 minutes ago");
+  });
+
+  test("relative - ago - hour", () => {
+    expect(DateFormatter.relative(now, now.subtract(Duration.Hours(1)))).toEqual("1 hour ago");
+    expect(DateFormatter.relative(now, now.subtract(Duration.Hours(2)))).toEqual("2 hours ago");
+  });
+
+  test("relative - ago - day", () => {
+    expect(DateFormatter.relative(now, now.subtract(Duration.Days(1)))).toEqual("1 day ago");
+    expect(DateFormatter.relative(now, now.subtract(Duration.Days(2)))).toEqual("2 days ago");
+  });
+
+  test("relative - in - minute", () => {
+    expect(DateFormatter.relative(now, now.add(Duration.Minutes(1)))).toEqual("in 1 minute");
+    expect(DateFormatter.relative(now, now.add(Duration.Minutes(2)))).toEqual("in 2 minutes");
+  });
+
+  test("relative - in - hour", () => {
+    expect(DateFormatter.relative(now, now.add(Duration.Hours(1)))).toEqual("in 1 hour");
+    expect(DateFormatter.relative(now, now.add(Duration.Hours(2)))).toEqual("in 2 hours");
+  });
+
+  test("relative - in - day", () => {
+    expect(DateFormatter.relative(now, now.add(Duration.Days(1)))).toEqual("in 1 day");
+    expect(DateFormatter.relative(now, now.add(Duration.Days(2)))).toEqual("in 2 days");
+  });
+
+  test("relative - with offset", () => {
+    expect(DateFormatter.relative(now, now.add(Duration.Hours(1)), offset)).toEqual("in 2 hours");
   });
 });
