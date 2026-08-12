b6d0384de3152fcc6ab4781347e8c55e44a9553b
Bartosz Gordon <gordonbartosz@gmail.com>
2026-08-12 13:50:04 +0200

    Make batch array readonly

--- src/batch.ts
+++ src/batch.ts
@@ -1,6 +1,6 @@
 import type { IntegerPositiveType } from "./integer-positive.vo";
 
-export function batch<T>(array: Array<T>, size: IntegerPositiveType): Array<Array<T>> {
+export function batch<T>(array: ReadonlyArray<T>, size: IntegerPositiveType): Array<Array<T>> {
   return Array.from({ length: Math.ceil(array.length / size) }, (_, index) =>
     array.slice(index * size, (index + 1) * size),
   );
