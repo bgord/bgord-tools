2021f5fdbdd26afd937550d289ce9ffaa29bd6cf
Bartosz Gordon <gordonbartosz@gmail.com>
2026-08-12 13:54:26 +0200

    Improve mime value regex

--- src/mime-value.vo.ts
+++ src/mime-value.vo.ts
@@ -3,7 +3,7 @@ import * as v from "valibot";
 export const MimeValueError = { Type: "mime.value.type", Invalid: "mime.value.invalid" };
 
 // One to twenty four lowercase letters, asterisk or hyphen, forward slash, one to seventy two lowercase letters, digits, asterisk, hyphen, plus, dot or underscore
-const MIME_VALUE_CHARS_WHITELIST = /^[a-z*-]{1,24}\/[a-z0-9*+-_.]{1,72}$/;
+const MIME_VALUE_CHARS_WHITELIST = /^[a-z*-]{1,24}\/[a-z0-9*+._-]{1,72}$/;
 
 export const MimeValue = v.pipe(
   v.string(MimeValueError.Type),
